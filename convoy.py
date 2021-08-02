826be8fe2975d3ffc381a77afcb72615a1f25f58
Suslov Igor <33933082+rand0mizator@users.noreply.github.com>
2021-08-02 16:58:22 +0700

    Update convoy.py
    stage 4\6
    exporting to:
    csv
    csv with edited cells
    sqlite db
    json

--- convoy.py
+++ convoy.py
@@ -1,68 +1,101 @@
 import pandas as pd
-import re
 import sqlite3
+import re
+import json
+from sqlalchemy import create_engine
+
 
 SHEET_NAME = 'Vehicles'
+xlsx_sequence = ['to_csv', 'to_checked_csv', 'to_s3db', 'to_json']
+csv_sequence = ['to_checked_csv', 'to_s3db', 'to_json']
+checked_csv_sequence = ['to_s3db', 'to_json']
+s3db_sequence = ['to_json']
 
 
-def get_name(file_name):
-    """
-    takes file name with extension and returns meaningfull part
-    example: convoy.xlsx -> convoy
-    """
-    if '[CHECKED]' in file_name:
-        return file_name.split('[CHECKED]'[0])
-    else:
-        return file_name.split('.')[0]
+def convert_to_csv(f_name, df):
+    name = f_name.split('.')[0]
+    rows_count = df.shape[0]
+    df.to_csv(fr'{name}.csv', index=None, header=True)
+    print(f"{rows_count} {'lines were' if rows_count > 1 else 'line was'} imported to {name}.csv")
 
 
-def create_table(file_name: str):
-    db_name = get_name(file_name) + '.s3db'
-    conn = sqlite3.connect(db_name)
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS convoy (
-                vehicle_id INTEGER PRIMARY KEY)
-                """)
-    conn.commit()
-    conn.close()
-
-
-def add_columns_to_db(file_name: str, column_names: list):
-    db_name = get_name(file_name) + '.s3db'
-    conn = sqlite3.connect(db_name)
-    cur = conn.cursor()
-    for column_name in column_names:
-        cur.execute("""ALTER TABLE convoy 
-                       ADD COLUMN ? INTEGER""", column_name)
-    conn.commit()
-    conn.close()
-
-
-def editor(file_name):
-    if file_name.endswith('.xlsx'):
-        file_type = '.xlsx'
-        my_df = pd.read_excel(file_name, sheet_name=SHEET_NAME, dtype=str)
-        file_name = file_name[:-5]
-    elif file_name.endswith('.csv'):
-        file_type = '.csv'
-        my_df = pd.read_csv(file_name, dtype=str)
-        file_name = file_name[:-4]
-    rows_count = my_df.shape[0]
-    my_df.to_csv(fr'{file_name}.csv', index=None, header=True)
-    if file_type == '.xlsx':
-        print(f"{rows_count} {'lines were' if rows_count > 1 else 'line was'} imported to {file_name}.csv")
+def convert_to_checked_csv(f_name, df):
+    name = f_name.split('.')[0]
     corrected_cells_count = 0
-    for index in my_df.index:
-        for column in my_df.columns:
-            line1 = my_df.loc[index, column]
-            line2 = re.sub(r'\D', '', my_df.loc[index, column])
+    for index in df.index:
+        for column in df.columns:
+            line1 = df.loc[index, column]
+            line2 = re.sub(r'\D', '', df.loc[index, column])
             if line1 != line2:
-                my_df.loc[index, column] = re.sub(r'\D', '', my_df.loc[index, column])
+                df.loc[index, column] = re.sub(r'\D', '', df.loc[index, column])
                 corrected_cells_count += 1
-    my_df.to_csv(fr'{file_name}[CHECKED].csv', index=None, header=True)
-    print(f"{corrected_cells_count} {'cells were' if corrected_cells_count > 1 else 'cell was'} corrected in {file_name}[CHECKED].csv")
+    df.to_csv(fr'{name}[CHECKED].csv', index=None, header=True)
+    print(f"{corrected_cells_count} {'cells were' if corrected_cells_count > 1 else 'cell was'} corrected in {name}[CHECKED].csv")
+    return df
+
+
+def convert_to_s3db(f_name, df):
+    if '[CHECKED]' in f_name:
+        db_name = f_name.split('[CHECKED]')[0] + '.s3db'
+    else:
+        db_name = f_name.split('.')[0] + '.s3db'
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS convoy (
+                vehicle_id INTEGER PRIMARY KEY,
+                engine_capacity INTEGER NOT NULL,
+                fuel_consumption INTEGER NOT NULL,
+                maximum_load INTEGER NOT NULL)
+                """)
+    con.commit()
+    rows_count = df.shape[0]
+    df.to_sql('convoy', con=con, if_exists='append', index=False)
+    print(f"{rows_count} {'records were' if rows_count > 1 else 'record was'} inserted to {db_name}")
+    con.commit()
+    con.close()
+
+
+def convert_to_json(f_name, df):
+    if '[CHECKED]' in f_name:
+        name = f_name.split('[CHECKED]')[0]
+    else:
+        name = f_name.split('.')[0]
+    rows_count = df.shape[0]
+    json_string = df.to_json(orient='records', indent=4)
+    with open(name + '.json', 'w') as f:
+        f.write('{"convoy": ' + json_string + '}')
+    print(f"{rows_count} {'vehicles were' if rows_count > 1 else 'vehicle was'} saved to {name}.json")
+
+
+def converting_conveyor(sequence, f_name, df):
+    if 'to_csv' in sequence:
+        convert_to_csv(f_name, df)
+    if 'to_checked_csv' in sequence:
+        df = convert_to_checked_csv(f_name, df)
+    if 'to_s3db' in sequence:
+        convert_to_s3db(f_name, df)
+    if 'to_json' in sequence:
+        convert_to_json(f_name, df)
+
+
+def main(f_name: str):
+    if f_name.endswith('.xlsx'):
+        df = pd.read_excel(f_name, sheet_name=SHEET_NAME, dtype=str)
+        converting_conveyor(xlsx_sequence, f_name, df)
+    elif f_name.endswith('.s3db'):
+        con = create_engine('sqlite:///' + f_name)
+        df = pd.read_sql('convoy', con)
+        converting_conveyor(s3db_sequence, f_name, df)
+    elif f_name.endswith('.csv'):
+        df = pd.read_csv(f_name, dtype=str)
+        if '[CHECKED]' in f_name:
+            converting_conveyor(checked_csv_sequence, f_name, df)
+        else:
+            converting_conveyor(csv_sequence, f_name, df)
+    else:
+        print(f"Something went wrong. {f_name}")
 
 
 print("Input file name")
 file_name = input()
-editor(file_name)
+main(file_name)
