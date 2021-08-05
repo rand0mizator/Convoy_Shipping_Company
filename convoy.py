dc1fbe48e2e7b1cb4a8203cb9bf136bf67b171f8
Suslov Igor <33933082+rand0mizator@users.noreply.github.com>
2021-08-05 15:37:53 +0700

    Update convoy.py
    Stage 5\6
    Added export to xml using pandas. pandas=>1.3.0 needed

--- convoy.py
+++ convoy.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import sqlite3
 import re
-import json
 from sqlalchemy import create_engine
 
 
 SHEET_NAME = 'Vehicles'
-xlsx_sequence = ['to_csv', 'to_checked_csv', 'to_s3db', 'to_json']
-csv_sequence = ['to_checked_csv', 'to_s3db', 'to_json']
-checked_csv_sequence = ['to_s3db', 'to_json']
-s3db_sequence = ['to_json']
+
+# sequences for conveyor. In which order provide converting according to input file extension
+xlsx_sequence = ['to_csv', 'to_checked_csv', 'to_s3db', 'to_json', 'to_xml']
+csv_sequence = ['to_checked_csv', 'to_s3db', 'to_json', 'to_xml']
+checked_csv_sequence = ['to_s3db', 'to_json', 'to_xml']
+s3db_sequence = ['to_json', 'to_xml']
 
 
 def convert_to_csv(f_name, df):
@@ -25,7 +26,7 @@ def convert_to_checked_csv(f_name, df):
     for index in df.index:
         for column in df.columns:
             line1 = df.loc[index, column]
-            line2 = re.sub(r'\D', '', df.loc[index, column])
+            line2 = re.sub(r'\D', '', df.loc[index, column])  # \D - filters all symbols except digits
             if line1 != line2:
                 df.loc[index, column] = re.sub(r'\D', '', df.loc[index, column])
                 corrected_cells_count += 1
@@ -35,6 +36,7 @@ def convert_to_checked_csv(f_name, df):
 
 
 def convert_to_s3db(f_name, df):
+    """Convert dataframe to s3db using sqlite3 library"""
     if '[CHECKED]' in f_name:
         db_name = f_name.split('[CHECKED]')[0] + '.s3db'
     else:
@@ -61,13 +63,25 @@ def convert_to_json(f_name, df):
     else:
         name = f_name.split('.')[0]
     rows_count = df.shape[0]
-    json_string = df.to_json(orient='records', indent=4)
+    json_string = df.to_json(orient='records', indent=4)  # dataframe -> json string
     with open(name + '.json', 'w') as f:
         f.write('{"convoy": ' + json_string + '}')
     print(f"{rows_count} {'vehicles were' if rows_count > 1 else 'vehicle was'} saved to {name}.json")
 
 
+def convert_to_xml(f_name, df):
+    """Converts pandas dataframe to xml. Important parameters: index, root_name, row_name, xml_declaration"""
+    if '[CHECKED]' in f_name:
+        name = f_name.split('[CHECKED]')[0]
+    else:
+        name = f_name.split('.')[0]
+    rows_count = df.shape[0]
+    df.to_xml(f"{name}.xml", index=False, root_name='convoy', row_name='vehicle', xml_declaration=False)
+    print(f"{rows_count} {'vehicles were' if rows_count > 1 else 'vehicle was'} saved to {name}.xml")
+
+
 def converting_conveyor(sequence, f_name, df):
+    """Iterates over provided sequence and makes needed convertings"""
     if 'to_csv' in sequence:
         convert_to_csv(f_name, df)
     if 'to_checked_csv' in sequence:
@@ -76,6 +90,8 @@ def converting_conveyor(sequence, f_name, df):
         convert_to_s3db(f_name, df)
     if 'to_json' in sequence:
         convert_to_json(f_name, df)
+    if 'to_xml' in sequence:
+        convert_to_xml(f_name, df)
 
 
 def main(f_name: str):
@@ -83,6 +99,7 @@ def main(f_name: str):
         df = pd.read_excel(f_name, sheet_name=SHEET_NAME, dtype=str)
         converting_conveyor(xlsx_sequence, f_name, df)
     elif f_name.endswith('.s3db'):
+        # read database using SQLALCHEMY engine
         con = create_engine('sqlite:///' + f_name)
         df = pd.read_sql('convoy', con)
         converting_conveyor(s3db_sequence, f_name, df)
